ee50d551087ea63ed922a3fb43589b13ee014422
auroratide <tf.auroratide@gmail.com>
2018-12-08 18:49:23 -0600

    include null and undefined as empty cases

--- test/Parser.spec.js
+++ test/Parser.spec.js
@@ -1,8 +1,10 @@
 const Parser = require('../lib/Parser');
 
 describe('Parser', () => {
-  it('should return empty array when input string is empty', () => {
+  it('should return empty array when input is empty', () => {
     const parser = new Parser();
     expect(parser.parse('')).toEqual([]);
+    expect(parser.parse(null)).toEqual([]);
+    expect(parser.parse(undefined)).toEqual([]);
   });
 });
